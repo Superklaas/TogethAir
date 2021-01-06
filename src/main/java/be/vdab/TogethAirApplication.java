487304d74318ebaba00dec9dff992d7e3fb6d621
Klaas Decock <klaasdc@icloud.com>
2021-01-06 21:23:35 +0100

    first commit

---
+++ src/main/java/be/vdab/TogethAirApplication.java
@@ -0,0 +1,13 @@
+package be.vdab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogethAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogethAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/TogethAirApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogethAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

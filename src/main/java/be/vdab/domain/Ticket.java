adc8eb039bbab2f672477960c30269954f66b24f
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:11:06 +0100

    Ticket created

--- src/main/java/be/vdab/domain/Ticket.java
+++ src/main/java/be/vdab/domain/Ticket.java
@@ -1,4 +1,29 @@
 package be.vdab.domain;
 
-public class Ticket {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Ticket extends AbstractEntity {
+
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private TravelClass travelClass;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Flight flight;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Passenger passenger;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Booking booking;
+
 }

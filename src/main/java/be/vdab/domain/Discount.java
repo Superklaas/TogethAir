424389a07f48ed749006454b1492800456c13fa9
Klaas Decock <klaasdc@icloud.com>
2021-01-08 13:56:30 +0100

    Discount created

--- src/main/java/be/vdab/domain/Discount.java
+++ src/main/java/be/vdab/domain/Discount.java
@@ -1,4 +1,35 @@
 package be.vdab.domain;
 
-public class Discount {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Discount extends AbstractEntity {
+
+    private final int togethAirRate = 5;
+
+    @NonNull
+    private TimeCategory timeCategory;
+
+    @NonNull
+    @ElementCollection
+    @MapKeyEnumerated(EnumType.STRING)
+    private Map<TravelClass,Integer> travelClassRate;
+
+    @NonNull
+    @ElementCollection
+    private Map<Integer,Integer> volumeDiscount;
+
+    @OneToMany(mappedBy = "discount")
+    private List<Flight> flights;
+
 }

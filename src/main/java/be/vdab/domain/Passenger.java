5eff693b0577e04b550d8916353f0e450784a54b
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:23:15 +0100

    Passenger created

--- src/main/java/be/vdab/domain/Passenger.java
+++ src/main/java/be/vdab/domain/Passenger.java
@@ -1,4 +1,25 @@
 package be.vdab.domain;
 
-public class Passenger {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Passenger extends AbstractEntity {
+
+    @NonNull
+    private String name;
+
+    @OneToMany(mappedBy = "passenger",cascade = CascadeType.REFRESH)
+    private List<Ticket> tickets;
+
 }

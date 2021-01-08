fa93db0f8fe8c3c78cd6a7a925c034a95e4e55e7
Klaas Decock <klaasdc@icloud.com>
2021-01-08 10:14:03 +0100

    Flight created

--- src/main/java/be/vdab/domain/Flight.java
+++ src/main/java/be/vdab/domain/Flight.java
@@ -1,4 +1,49 @@
 package be.vdab.domain;
 
-public class Flight {
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Digits;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Flight extends AbstractEntity {
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Location departure;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Location destination;
+
+    @NonNull
+    private LocalDateTime departureTime;
+
+    @NonNull
+    private LocalDateTime arrivalTime;
+
+    @NonNull
+    @Digits(integer = 5, fraction = 2)
+    private double basePrice;
+
+    @ElementCollection
+    @MapKeyEnumerated(EnumType.STRING)
+    private Map<TravelClass,Integer> bookedSeats;
+
+    @ManyToOne(cascade = CascadeType., fetch = FetchType.LAZY)
+    private Airplane airplane;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private Airline airline;
+
+    @ManyToOne(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private Discount discount;
+
+    @OneToMany(mappedBy = "flight", cascade = CascadeType.PERSIST)
+    private List<Ticket> tickets;
+
 }

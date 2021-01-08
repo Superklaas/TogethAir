f1bffa453f86374ed24239a18710cd10a16dde3e
Klaas Decock <klaasdc@icloud.com>
2021-01-08 11:17:37 +0100

    Airport created

--- src/main/java/be/vdab/domain/Airport.java
+++ src/main/java/be/vdab/domain/Airport.java
@@ -1,4 +1,38 @@
 package be.vdab.domain;
 
-public class Airport {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Airport extends AbstractEntity {
+
+    @NonNull
+    private String name;
+
+    @NonNull
+    private String city;
+
+    private String region;
+
+    private String country;
+
+    @NonNull
+    private String airportCode;
+
+    @OneToMany(mappedBy = "departure", cascade = CascadeType.REFRESH)
+    private List<Flight> departingFlights;
+
+    @OneToMany(mappedBy = "destination", cascade = CascadeType.REFRESH)
+    private List<Flight> arrivingFlights;
+
 }

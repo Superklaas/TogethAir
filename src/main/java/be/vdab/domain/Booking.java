fa3f95f8111994869401b0be0c5daa6f3c113588
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:38:47 +0100

    Booking created

--- src/main/java/be/vdab/domain/Booking.java
+++ src/main/java/be/vdab/domain/Booking.java
@@ -1,4 +1,38 @@
 package be.vdab.domain;
 
-public class Booking {
+import be.vdab.domain.user.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Booking extends AbstractEntity {
+
+    @NonNull
+    private LocalDate dateBooking;
+
+    @NonNull
+    private double priceBooking;
+
+    @NonNull
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @NonNull
+    private boolean isPayed;
+
+    @OneToMany(mappedBy = "booking", cascade = CascadeType.PERSIST)
+    private List<Ticket> tickets;
+
+    @ManyToOne(cascade = CascadeType.REFRESH, fetch = FetchType.LAZY)
+    private Customer customer;
+
 }

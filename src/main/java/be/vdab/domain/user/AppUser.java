62b49909877b5e91479f827342dc8e1131fff8a0
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:43:40 +0100

    AppUser created

--- src/main/java/be/vdab/domain/user/AppUser.java
+++ src/main/java/be/vdab/domain/user/AppUser.java
@@ -1,4 +1,20 @@
 package be.vdab.domain.user;
 
-public abstract class AppUser {
+import be.vdab.domain.AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public abstract class AppUser extends AbstractEntity {
+
+    @NonNull
+    private String name;
+
 }

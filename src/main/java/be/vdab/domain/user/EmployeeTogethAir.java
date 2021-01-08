b30a5ebea63725acb766133220bcfcc19d55dd39
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:52:04 +0100

    EmployeeTogethAir created

--- src/main/java/be/vdab/domain/user/EmployeeTogethAir.java
+++ src/main/java/be/vdab/domain/user/EmployeeTogethAir.java
@@ -1,4 +1,19 @@
 package be.vdab.domain.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class EmployeeTogethAir extends AppUser {
+
+    @NonNull
+    private int employeeNumber;
+
 }

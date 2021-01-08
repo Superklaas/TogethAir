242c096e9620919b8b7f9c2c5b045bdeebf013f7
Klaas Decock <klaasdc@icloud.com>
2021-01-08 14:47:54 +0100

    Customer created

--- src/main/java/be/vdab/domain/user/Customer.java
+++ src/main/java/be/vdab/domain/user/Customer.java
@@ -1,4 +1,26 @@
 package be.vdab.domain.user;
 
+import be.vdab.domain.Booking;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Customer extends AppUser {
+
+    @NonNull
+    private int customerNumber;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    List<Booking> bookings;
+
 }

274ad4f725f1f008c984fabd7ea7da84e6499a9a
Klaas Decock <klaasdc@icloud.com>
2021-01-08 11:08:57 +0100

    Airplane created

--- src/main/java/be/vdab/domain/Airplane.java
+++ src/main/java/be/vdab/domain/Airplane.java
@@ -1,4 +1,35 @@
 package be.vdab.domain;
 
-public class Airplane {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Map;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class Airplane extends AbstractEntity {
+
+    @NonNull
+    private String brand;
+
+    @NonNull
+    private String model;
+
+    @NonNull
+    @ElementCollection
+    @MapKeyEnumerated(EnumType.STRING)
+    private Map<TravelClass,Integer> capacity;
+
+    @ManyToMany(mappedBy = "airplanes", cascade = CascadeType.REFRESH)
+    private List<Airline> airlines;
+
+    @OneToMany(mappedBy = "airplane", cascade = CascadeType.REFRESH)
+    private List<Flight> flights;
+
 }

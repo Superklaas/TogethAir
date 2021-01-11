001663672c9f1d20ace100c4660cd6723143b933
Klaas Decock <klaasdc@icloud.com>
2021-01-11 17:31:29 +0100

    credit card details added to Customer

--- src/main/java/be/vdab/domain/user/Customer.java
+++ src/main/java/be/vdab/domain/user/Customer.java
@@ -9,6 +9,7 @@ import lombok.NonNull;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @Entity
@@ -17,8 +18,13 @@ import java.util.List;
 @Getter
 public class Customer extends AppUser {
 
-    @NonNull
-    private int customerNumber;
+    @Pattern(regexp = "^[0-9]{4}[-\\.\\s]?[0-9]{4}[-\\.\\s]?[0-9]{4}[-\\.\\s]?[0-9]{4}$",
+            message = "credit card number not valid")
+    private String creditCardNumber;
+
+    @Pattern(regexp = "^[1-12][-\\.\\s\\/]?[0-9]{2}$",
+            message = "expiration date not valid")
+    private String expirationDate;
 
     @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
     List<Booking> bookings;

a9176f10133c3c243807f5cb59e9d76c2c9ebe9e
Klaas Decock <klaasdc@icloud.com>
2021-01-14 09:35:04 +0100

    FlightService: method searchFlights

---
+++ src/main/java/be/vdab/service/FlightService.java
@@ -0,0 +1,42 @@
+package be.vdab.service;
+
+import be.vdab.domain.Airline;
+import be.vdab.domain.Airport;
+import be.vdab.domain.Flight;
+import be.vdab.domain.TravelClass;
+import be.vdab.repository.AirlineRepository;
+import be.vdab.repository.AirportRepository;
+import be.vdab.repository.FlightRepository;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class FlightService {
+
+    private FlightRepository flightRepository;
+    private AirlineRepository airlineRepository;
+    private AirportRepository airportRepository;
+
+    public List<Flight> readAllFlights() {
+        return flightRepository.findAll();
+    }
+
+    public List<Flight> searchFlights(String airlineName, String departureName,
+                                      String destinationName, LocalDate dateDeparture) {
+        Airline airline = airlineRepository.findAirlineByNameContaining(airlineName);
+        Airport departure = airportRepository.findAirportByNameContaining(departureName);
+        Airport destination = airportRepository.findAirportByNameContaining(destinationName);
+        LocalDateTime startOfDay = dateDeparture.atStartOfDay();
+        LocalDateTime endOfDay = dateDeparture.atTime(23,59);
+        return flightRepository.findFlightByAirlineAndDepartureAndDestinationAndDepartureTimeBetween(
+                        airline,departure,destination,startOfDay,endOfDay);
+    }
+
+}

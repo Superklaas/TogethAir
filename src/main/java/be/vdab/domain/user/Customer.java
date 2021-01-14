1adeedbbb56b46e3f7622390d0a0c211a300f62d
Klaas Decock <klaasdc@icloud.com>
2021-01-14 09:33:17 +0100

    Customer: changed @Pattern expirationDate

--- src/main/java/be/vdab/domain/user/Customer.java
+++ src/main/java/be/vdab/domain/user/Customer.java
@@ -1,10 +1,7 @@
 package be.vdab.domain.user;
 
 import be.vdab.domain.Booking;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -16,13 +13,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@Setter
 public class Customer extends AppUser {
 
     @Pattern(regexp = "^[0-9]{4}[-\\.\\s]?[0-9]{4}[-\\.\\s]?[0-9]{4}[-\\.\\s]?[0-9]{4}$",
             message = "credit card number not valid")
     private String creditCardNumber;
 
-    @Pattern(regexp = "^[1-12][-\\.\\s\\/]?[0-9]{2}$",
+    @Pattern(regexp = "(0[1-9]|1[0-2])\\/([0-9]{2})$",
             message = "expiration date not valid")
     private String expirationDate;
 

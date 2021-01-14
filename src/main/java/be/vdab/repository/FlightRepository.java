97c3a9c8eb8cf38ac30c7670b0535e9e8459066a
Klaas Decock <klaasdc@icloud.com>
2021-01-14 09:37:29 +0100

    FlightRepository: findFlightByAirlineAndDepartureAndDestinationAndDepartureTimeBetween

--- src/main/java/be/vdab/repository/FlightRepository.java
+++ src/main/java/be/vdab/repository/FlightRepository.java
@@ -1,8 +1,18 @@
 package be.vdab.repository;
 
+import be.vdab.domain.Airline;
+import be.vdab.domain.Airport;
 import be.vdab.domain.Flight;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight,Integer> {
+
+    public List<Flight> findFlightByAirlineAndDepartureAndDestinationAndDepartureTimeBetween(
+            Airline airline, Airport departure, Airport destination,LocalDateTime startOfDay,LocalDateTime endOfDay);
 
 }

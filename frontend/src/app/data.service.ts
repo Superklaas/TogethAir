be66a99c48c9378c8685dbb975b21cdcfe1fb510
Klaas Decock <klaasdc@icloud.com>
2021-01-20 18:54:00 +0100

    flightUrl added

--- frontend/src/app/data.service.ts
+++ frontend/src/app/data.service.ts
@@ -9,11 +9,13 @@ import {Observable} from "rxjs";
 })
 export class DataService {
 
+  private flightUrl = 'http://localhost:8082/flights';
+
   constructor(private http: HttpClient) {
   }
 
   getFlightById(id: number) : Observable<Flight> {
-    return this.http.get<Flight>('http://localhost:8082/flights/getFlightById/' + id);
+    return this.http.get<Flight>(this.flightUrl + '/getFlightById/' + id);
   }
 
 }
